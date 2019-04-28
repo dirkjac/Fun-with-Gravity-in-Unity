4190bf432b67714a15336fce586f782f659c1095
dirkjacobasch <dirk.jacobasch@outlook.com>
2019-04-28 13:36:48 +0200

    First import

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class controls the cameras rotation, zoom and so on.
+/// Zoom: Scroll-Wheel / E & Q
+/// Move up: Arrow Up
+/// Move Down: Arrow down
+/// Move left: Arrow left
+/// Move right: Arrow right
+/// Hold left mouse button to move camera
+/// </summary>
+public class CameraScript : MonoBehaviour {
+
+    [Tooltip("Defines the cameras maximum field of view.")]
+    [Range(60, 150)]
+	[SerializeField] private float maxFOV = 100f;
+
+    [Tooltip("Defines the cameras minimum field of view.")]
+    [Range(20, 59)]
+	[SerializeField] private float minFOV = 40f;
+
+    [Tooltip("Defines how fast the camera zoom in and out.")]
+    [Range(5f, 50f)]
+	[SerializeField] private float zoomSpeed = 40f;
+
+	[SerializeField] private GameObject centerObject = null;
+
+    private float scrollDir = 0f;
+    private float cameraFOV = 60f;
+    private float mouseXDir = 0f;
+
+    [Range(1, 50)]
+    [SerializeField] private float mouseSpeed = 25f;
+
+	private Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+		mainCam = Camera.main;
+
+		mainCam.fieldOfView = maxFOV;
+	}
+	
+	/// <summary>
+    /// Camera movement.
+    /// </summary>
+	void Update () {
+        scrollDir = Input.GetAxis("Mouse ScrollWheel");
+        cameraFOV = mainCam.fieldOfView;
+
+        // Zoom in / out
+        if (scrollDir > 0f) {
+            if (cameraFOV > minFOV) {
+                cameraFOV -= Time.deltaTime * zoomSpeed;
+				mainCam.fieldOfView = cameraFOV;
+            }
+
+        } else if (scrollDir < 0f) {
+            if (cameraFOV < maxFOV) {
+                cameraFOV += Time.deltaTime * zoomSpeed;
+				mainCam.fieldOfView = cameraFOV;
+            }
+        }
+
+        // Zoom with keyboard
+        if (Input.GetKey(KeyCode.E)) {
+            if (cameraFOV > minFOV) {
+                cameraFOV -= Time.deltaTime * zoomSpeed;
+				mainCam.fieldOfView = cameraFOV;
+
+            } else {
+				mainCam.fieldOfView = minFOV;
+            }
+
+        } else if (Input.GetKey(KeyCode.Q)) {
+            if (cameraFOV < maxFOV) {
+                cameraFOV += Time.deltaTime * zoomSpeed;
+				mainCam.fieldOfView = cameraFOV;
+
+            } else {
+				mainCam.fieldOfView = maxFOV;
+            }
+        }
+
+        // Orbit with keys
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.RotateAround(centerObject.transform.position, Vector3.right, mouseSpeed * Time.deltaTime);
+
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.RotateAround(centerObject.transform.position, Vector3.left, mouseSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.RotateAround(centerObject.transform.position, Vector3.down, mouseSpeed * Time.deltaTime);
+
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.RotateAround(centerObject.transform.position, Vector3.up, mouseSpeed * Time.deltaTime);
+        }
+
+        // Orbit with mouse
+        if (Input.GetButton("Fire1")) {
+			if (Input.GetAxis("Mouse X") != 0f) {
+				mouseXDir = Input.GetAxis("Mouse X");
+				if (mouseXDir > 0f) {
+					transform.RotateAround(centerObject.transform.position, Vector3.down, mouseSpeed * Time.deltaTime);
+
+				} else if (mouseXDir < 0f) {
+					transform.RotateAround(centerObject.transform.position, Vector3.down, -mouseSpeed * Time.deltaTime);
+				}
+			}
+
+			if (Input.GetAxis("Mouse Y") != 0f) {
+				mouseXDir = Input.GetAxis("Mouse Y");
+				if (mouseXDir > 0f) {
+					transform.RotateAround(centerObject.transform.position, Vector3.left, mouseSpeed * Time.deltaTime);
+
+				} else if (mouseXDir < 0f) {
+					transform.RotateAround(centerObject.transform.position, Vector3.left, -mouseSpeed * Time.deltaTime);
+				}
+			}
+		}
+
+        // Quit application
+        if (Input.GetKey(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class spawns the orbiting objects.
+/// </summary>
+public class SpawnScript : MonoBehaviour {
+
+	[SerializeField] private GameObject planetPrefab = null;
+	[SerializeField] private GameObject sun = null;
+	[SerializeField] private GameObject sunBlue = null;
+	[SerializeField] private GameObject sunGreen = null;
+	[SerializeField] private GameObject centerObject = null;
+
+	private Vector3 sunVelocity = Vector3.zero;
+	private Vector3 sunBlueVelocity = Vector3.zero;
+	private Vector3 sunGreenVelocity = Vector3.zero;
+
+    [Tooltip("Defines the number of orbiting objects to spawn. If you have performance problems, decrease this number.")]
+    [Range(1, 500)]
+	[SerializeField] private int numberOfPlanets = 1;
+
+    [Tooltip("The radius around the center where orbiting objects are spawned.")]
+    [Range(10, 10000)]
+	[SerializeField] private float radius = 20f;
+
+    [Tooltip("The minimum mass of orbiting objects. Play around with it to see how the mass influence the orbit behaviour of objects.")]
+    [Range(0, 10000)]
+	[SerializeField] private float minPlanetMass = 500f;
+
+    [Tooltip("The maximum mass of orbiting objects. Play around with it to see how the mass influence the orbit behaviour of objects.")]
+    [Range(10000, 100000)]
+	[SerializeField] private float maxPlanetMass = 50000f;
+
+    [Tooltip("The delay between spawning new orbiting objects at game start.")]
+    [Range(0, 10)]
+	[SerializeField] private float spawnDelay = 1f;
+
+    [Tooltip("The maximum size of the orbiting objects. This value does not influence the mass of an orbiting object.")]
+    [Range(1f, 20f)]
+	[SerializeField] private float maxPlanetSize = 1f;
+
+    private List<GameObject> planets = new List<GameObject>();
+	private Transform cameraTransform;
+
+    /// <summary>
+    /// Spawn orbiting objects.
+    /// </summary>
+    void Start () {
+		cameraTransform = Camera.main.transform;
+
+		StartCoroutine(SpawnPlanets());
+
+        sun.GetComponent<Rigidbody>().velocity = sunVelocity;
+        sunBlue.GetComponent<Rigidbody>().velocity = sunBlueVelocity;
+        sunGreen.GetComponent<Rigidbody>().velocity = sunGreenVelocity;
+    }
+
+	/// <summary>
+	/// Spawn new objects with a delay.
+	/// </summary>
+	/// <returns></returns>
+    private IEnumerator SpawnPlanets() {
+        for (int i = 0; i < numberOfPlanets; i++) {
+            GameObject planet = Instantiate(planetPrefab);
+            float x = -radius / 2f + Random.value * radius;
+            float y = -radius / 2f + Random.value * radius;
+            float z = -radius / 2f + Random.value * radius;
+            Vector3 position = new Vector3(x, y, z);
+            planet.transform.position = position;
+            float scale = 1f + Random.value * maxPlanetSize;
+            planet.transform.localScale = new Vector3(scale, scale, scale);
+
+            float m = minPlanetMass + Random.value * (maxPlanetMass - minPlanetMass);
+            Rigidbody prb = planet.GetComponent<Rigidbody>();
+            prb.mass = m;
+            prb.velocity = new Vector3(Random.value * 15f, Random.value * 30f, Random.value * 5f);
+            planet.GetComponent<Renderer>().material.color = new Color(0.5f + Random.value / 2, 0.5f + Random.value / 2, 0.5f + Random.value / 2);
+
+            planets.Add(planet);
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    /// <summary>
+    /// Spawn new planet at a random position with a random velocity.
+    /// </summary>
+    public void SpawnNewPanet() {
+        GameObject planet = Instantiate(planetPrefab);
+        float x = -radius / 2f + Random.value * radius;
+        float y = -radius / 2f + Random.value * radius;
+        float z = -radius / 2f + Random.value * radius;
+        Vector3 position = new Vector3(x, y, z);
+        planet.transform.position = position;
+        float scale = 1f + Random.value * maxPlanetSize;
+        planet.transform.localScale = new Vector3(scale, scale, scale);
+
+        float m = minPlanetMass + Random.value * (maxPlanetMass - minPlanetMass);
+        Rigidbody prb = planet.GetComponent<Rigidbody>();
+        if (prb) {
+            prb.mass = m;
+            prb.velocity = new Vector3(Random.value * 15f, Random.value * 30f, Random.value * 5f);
+        }
+
+        planet.GetComponent<Renderer>().material.color = new Color(0.5f + Random.value/2, 0.5f + Random.value / 2, 0.5f + Random.value / 2);
+
+        planets.Add(planet);
+    }
+
+    /// <summary>
+    /// Let camera always look at the center object.
+    /// </summary>
+    private void Update() {
+        if (centerObject != null) {
+            cameraTransform.LookAt(centerObject.transform);
+        }
+    }
+}
